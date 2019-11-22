82687c76adcfc30d253584aea1299f4704a555a9
kwonhygge <kitteniso1103@gmail.com>
2019-11-23 00:13:39 +0900

    make event-delegations

--- js/main.js
+++ js/main.js
@@ -1,29 +1,75 @@
-const heart = document.querySelector('.heart_btn');
 const header = document.querySelector('#header');
 const side_box = document.querySelector('.side_box');
+const variableWidth = document.querySelectorAll('.contents_box .contents');
+const delegation = document.querySelector('.contents_box');
 
-heart.addEventListener('click',function(){
-  heart.classList.toggle('on');
-});
+function delegationFunc(e){
+  let elem = e.target;
+  console.log(e.target);
 
+  while(!elem.getAttribute('data-name')){
+    elem = elem.parentNode;
+
+    if (elem.nodeName === 'BODY'){
+      elem=null;
+      return;
+    }
+  }
+
+  if (elem.matches('[data-name="heartbeat"]')){
+
+  }else if (elem.matches('[data-name="bookmark"]')) {
+
+  }else if (elem.matches('[data-name="share"]')) {
+
+  }else if (elem.matches('[data-name="more"]')) {
+
+  }
+  elem.classList.toggle('on');
+}
 function resizeFunc(){
   if (pageYOffset >= 10){
     let calcWidth = (window.innerWidth * 0.5) + 167;
     side_box.style.left = calcWidth + 'px';
   }
+  if (matchMedia('screen and (max-width:800px)').matches){
+
+    for(let i=0; i<variableWidth.length; i++){
+      variableWidth[i].style.width = window.innerWidth -20 + 'px';
+
+    }
+  }else{
+    for(let i=0; i<variableWidth.length; i++){
+      if(window.innerWidth>600){
+        variableWidth[i].removeAttribute('style');
+      }
+
+    }
+  }
+
 }
 
 function scrollFunc(){
   if (pageYOffset>=10){
     header.classList.add('on');
-    side_box.classList.add('on');
+
+    if(side_box){
+      side_box.classList.add('on');
+    }
 
     resizeFunc();
   }else{
     header.classList.remove('on');
-    side_box.classList.remove('on');
-    side_box.removeAttribute('style');
+    if(side_box){
+      side_box.classList.remove('on');
+      side_box.removeAttribute('style');
+    }
   }
 }
 
+if(delegation){
+  delegation.addEventListener('click',delegationFunc);
+}
+
+window.addEventListener('resize',resizeFunc);
 window.addEventListener('scroll',scrollFunc);
