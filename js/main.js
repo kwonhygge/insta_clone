c0751051a70c8e83c5f121ff19a14bdd34b5d65a
kwonhygge <kitteniso1103@gmail.com>
2020-11-16 16:31:16 +0900

    Adds scroll event for main

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
 const hearts = document.querySelectorAll(".heart_btn");
+const header = document.querySelector("#header");
+const sidebox = document.querySelector(".side_box");
+
+const resizeFunc = () => {
+    if(pageYOffset>=10){
+    let calcWidth = (window.innerWidth * 0.5) + 170;
+    sidebox.style.left = calcWidth + "px";
+    }
+}
+
+const scrollFunc = () => {
+    if(pageYOffset>=10){
+        header.classList.add("on");
+        sidebox.classList.add("on");
+        resizeFunc();
+    }else{
+        header.classList.remove("on");
+        sidebox.classList.remove("on");
+        sidebox.removeAttribute("style");
+    }
+}
 
 hearts.forEach(heart => {
     heart.addEventListener("click",function(){
         heart.classList.toggle("on");
     })
 });
+
+window.addEventListener("scroll",scrollFunc);
