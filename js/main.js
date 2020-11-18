e74161afffa97281f13a15f4702afc7f33c50174
kwonhygge <kitteniso1103@gmail.com>
2020-11-18 14:20:56 +0900

    Adds resize event for contents box

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
 const hearts = document.querySelectorAll(".heart_btn");
 const header = document.querySelector("#header");
 const sidebox = document.querySelector(".side_box");
+const variableWidth = document.querySelectorAll(".contents_box .contents");
 
 const resizeFunc = () => {
     if(pageYOffset>=10){
     let calcWidth = (window.innerWidth * 0.5) + 170;
     sidebox.style.left = calcWidth + "px";
+    }
+    if(matchMedia('screen and (max-width: 800px').matches){
+
+        
+        for(let i=0; i<variableWidth.length; i++){
+            variableWidth[i].style.width = window.innerWidth - 20 + "px";
+        }
+    
+    
+    }else{
+       
+        for(let i=0; i<variableWidth.length; i++){
+            variableWidth[i].removeAttribute("style");
+        } 
+        
     }
 }
 
@@ -26,5 +42,5 @@ hearts.forEach(heart => {
         heart.classList.toggle("on");
     })
 });
-
+window.addEventListener("resize",resizeFunc);
 window.addEventListener("scroll",scrollFunc);
